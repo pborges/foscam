b96ddb2aa9b6fa3fbbfc0d9bd80a4091e8aaadcd
Paulo Borges <pborges475@gmail.com>
2015-12-22 09:25:42 -0800

    Add some device control functions

--- device.go
+++ device.go
@@ -1,8 +1,74 @@
 package foscam
 
-//ToDo: SetDevName
-//ToDo: GetDevName
-//ToDo: GetDevInfo
+import (
+	"encoding/xml"
+)
+
+type DeviceInfo struct {
+	CGIResult
+	ProductName  string `xml:"productName"`
+	SerialNumber string `xml:"serialNo"`
+	DeviceName   string `xml:"devName"`
+	MacAddress   string `xml:"mac"`
+	Year         string `xml:"year"`
+	Month        string `xml:"mon"`
+}
+
+func SetDevName(c Credentials, name string) (err error) {
+	params := make(map[string]string)
+	params["cmd"] = "setDevName"
+	params["devName"] = name
+
+	res, err := request(c, params)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	x := new(CGIResult)
+	err = xml.NewDecoder(res.Body).Decode(x)
+	if err != nil {
+		return
+	}
+	_, err = x.Success()
+	return
+}
+
+func GetDevName(c Credentials) (name string, err error) {
+	params := make(map[string]string)
+	params["cmd"] = "getDevName"
+
+	res, err := request(c, params)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	x := new(DeviceInfo)
+	err = xml.NewDecoder(res.Body).Decode(x)
+	if err != nil {
+		return
+	}
+	_, err = x.Success()
+	if err != nil {
+		return
+	}
+	name = x.DeviceName
+	return
+}
+
+func GetDevInfo(c Credentials) (info *DeviceInfo, err error) {
+	params := make(map[string]string)
+	params["cmd"] = "getDevInfo"
+
+	res, err := request(c, params)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	info = new(DeviceInfo)
+	xml.NewDecoder(res.Body).Decode(info)
+	return
+}
+
 //ToDo: SetSystemTime
 //ToDo: GetSystemTime
 //ToDo: getOSDSetting
--- device_test.go
+++ device_test.go
@@ -1,8 +1,44 @@
 package foscam
 
-//ToDo: TestSetDevName
-//ToDo: TestGetDevName
-//ToDo: TestGetDevInfo
+import "testing"
+
+func TestSetDevName(t *testing.T) {
+	c, err := LoadCredentials("testcredentials.json")
+	if err != nil {
+		t.Fatal("Unable to load credentials:", err.Error())
+	}
+
+	err = SetDevName(c, "test")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestGetDevName(t *testing.T) {
+	c, err := LoadCredentials("testcredentials.json")
+	if err != nil {
+		t.Fatal("Unable to load credentials:", err.Error())
+	}
+
+	name, err := GetDevName(c)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("Got Device Name " + name)
+}
+
+func TestGetDevInfo(t *testing.T) {
+	c, err := LoadCredentials("testcredentials.json")
+	if err != nil {
+		t.Fatal("Unable to load credentials:", err.Error())
+	}
+
+	info, err := GetDevInfo(c)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Got Device Name %+v\n", info)
+}
 //ToDo: TestSetSystemTime
 //ToDo: TestGetSystemTime
 //ToDo: TestgetOSDSetting
