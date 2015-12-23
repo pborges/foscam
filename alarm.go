7048eba6f4539ec55ea2916385ea37fe908924e9
Paulo Borges <pborges475@gmail.com>
2015-12-23 15:34:44 -0800

    Add endpoints for Set/GetAlarmRecordConfig

--- alarm.go
+++ alarm.go
@@ -1,4 +1,60 @@
 package foscam
 
-//ToDo: GetAlarmRecordConfig
-//ToDo: SetAlarmRecordConfig
+import (
+	"encoding/xml"
+	"strconv"
+	"errors"
+)
+
+type AlarmRecordConfig struct {
+	IsEnablePreRecord  int `xml:"isEnablePreRecord"`
+	PreRecordSecs      int `xml:"preRecordSecs"`
+	AlarmRecordSeconds int `xml:"alarmRecordSeconds"`
+}
+
+func GetAlarmRecordConfig(c Credentials) (settings *AlarmRecordConfig, err error) {
+	params := make(map[string]string)
+	params["cmd"] = "getAlarmRecordConfig"
+
+	res, err := request(c, params)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	settings = new(AlarmRecordConfig)
+	xml.NewDecoder(res.Body).Decode(settings)
+	return
+}
+
+func SetAlarmRecordConfig(c Credentials, settings AlarmRecordConfig) (err error) {
+	// validate AlarmRecordConfig
+	if !(settings.IsEnablePreRecord >= 0 && settings.IsEnablePreRecord <= 1) {
+		err = errors.New("IsEnablePreRecord must be 0 (disabled) or 1 (enabled)")
+		return
+	}
+	if !(settings.PreRecordSecs > 0 && settings.PreRecordSecs <= 5) {
+		err = errors.New("PreRecordSecs must be between 1 and 5")
+		return
+	}
+	switch settings.AlarmRecordSeconds {
+	case 10, 20, 30, 40, 50, 60, 120, 180, 240, 300:
+	default:
+		err = errors.New("AlarmRecordSeconds must be 10,20,30,40,50,60,120,180,240,300")
+		return
+	}
+	params := make(map[string]string)
+	params["cmd"] = "setAlarmRecordConfig"
+	params["isEnablePreRecord"] = strconv.Itoa(settings.IsEnablePreRecord)
+	params["preRecordSecs"] = strconv.Itoa(settings.PreRecordSecs)
+	params["alarmRecordSecs"] = strconv.Itoa(settings.AlarmRecordSeconds)
+
+	res, err := request(c, params)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	result := new(CGIResult)
+	xml.NewDecoder(res.Body).Decode(result)
+	_, err = result.Success()
+	return
+}
--- alarm_test.go
+++ alarm_test.go
@@ -1,4 +1,31 @@
 package foscam
 
-//ToDo: TestGetAlarmRecordConfig
-//ToDo: TestSetAlarmRecordConfig
+import "testing"
+
+func TestGetAlarmRecordConfig(t *testing.T) {
+	c, err := LoadCredentials("testcredentials.json")
+	if err != nil {
+		t.Fatal("Unable to load credentials:", err.Error())
+	}
+
+	_, err = GetAlarmRecordConfig(c)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSetAlarmRecordConfig(t *testing.T) {
+	c, err := LoadCredentials("testcredentials.json")
+	if err != nil {
+		t.Fatal("Unable to load credentials:", err.Error())
+	}
+
+	err = SetAlarmRecordConfig(c, AlarmRecordConfig{
+		IsEnablePreRecord:0,
+		PreRecordSecs:1,
+		AlarmRecordSeconds:20,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
